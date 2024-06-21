41706719a660417b6b4c21f03055b106a0f164ad
RoopikaNayani <roopika.nayani@kriyaglobalitservices.com>
2024-06-21 17:47:35 +0530

    task2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-  path: "dashboard",
-  loadChildren:()=> import("./logindetails/dashboard/dashboard.module").then(m=>m.DashboardModule)
-  },
-  {
-    path: "login",
-    loadChildren:()=> import("./logindetails/login/login.module").then(m=>m.LoginModule)
-    }
+    path: "dashboard",
+    loadChildren:()=> import("./logindetails/dashboard/dashboard.module").then(m=>m.DashboardModule)
+    },
+    {
+      path: "",
+      loadChildren:()=> import("./logindetails/login/login.module").then(m=>m.LoginModule)
+      }
 
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
